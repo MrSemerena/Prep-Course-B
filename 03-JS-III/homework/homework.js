c1e978afeeb2c86c86a00ba976823c173b637383
Jorge Miguel Semerena Núñez <jorge_semerena@icloud.com>
2022-10-28 14:59:35 -0500

    Tercera tarea

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,56 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    const numerArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let suma = 0;
+
+    for (let i = 0; i < numerArray.length; i += 1) {
+      suma += numerArray[i];
+    }
+
+    return suma;
+    return (imprimirSumaNumeros([]));
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  
+  let pares = array.filter((x) => x % 2 === 0) 
+  return pares;
+  
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var arrayCuadrado = [];
+
+  for (var i = 0; i < array.length; i ++) {
+    arrayCuadrado.push(array[i] ** 2);
+  }
+  return arrayCuadrado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var total = 0;
+
+  for (let i = 0; i < array.length; i ++) {
+    total += array[i];
+  }
+  return total;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  let digitos = String(num);
+  return digitos.length;
 }
   
   
